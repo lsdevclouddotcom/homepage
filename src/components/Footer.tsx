460d4c8d6974f62b0ee9a2cc5632e70a2d395ed3
lsdevcloud <info@lsdevcloud.com>
2024-09-07 17:32:44 +0200

    added footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,16 @@
 import { Component, ReactNode } from "react";
+import '../footer.css';
 
 export class Footer extends Component {
 
     render(): ReactNode {
 
         return <div id="footer">
+            <div id="footer-content">
 
+                <h2>This atrocity is <a href="https://github.com/lsdevclouddotcom/homepage">open source</a>.</h2>
+                <h2>Made with ☕ and 🍺.</h2>
+            </div>
         </div>
     }
 }
