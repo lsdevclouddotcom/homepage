06f272f87655eb0e4f2fa23c0fce0f0eac168e1b
lsdevcloud <info@lsdevcloud.com>
2024-09-07 17:20:00 +0200

    added footer and navbar

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import { Component, ReactNode } from "react";
+
+export class Footer extends Component {
+
+    render(): ReactNode {
+
+        return <div id="footer">
+
+        </div>
+    }
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,17 @@
 import { Component, ReactNode } from "react";
-
+import '../navbar.css'
 export class Navbar extends Component {
 
     render(): ReactNode {
 
-        return <div id="navbar"></div>
+        return <div id="navbar">
+
+            <div className="navbar-title">
+                <h1><a href="/">LSDevCloud.com</a></h1>
+            </div>
+            <h1 className="navbar-link"><a href="/contact">Contact</a></h1>
+            <h1 className="navbar-link"><a href="https://github.com/lsdevclouddotcom">GitHub</a></h1>
+        </div>
     }
 
 }
