964b2f1099e36c52d8c4b67c3e3e2daeaa6c0533
lsdevcloud <info@lsdevcloud.com>
2024-09-07 17:56:01 +0200

    KEKW

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,9 +1,22 @@
+import { useEffect } from "react"
+
 function Home() {
+
+    useEffect(() => {
+
+        // essential feature; required for this website to function AT ALL
+        const kekw = document.getElementById("kekw") as HTMLElement;
+        setTimeout(() => {
+            kekw.style.display = "none";
+        }, 2500);
+
+    });
 
     return (
         <div id="page">
             <h2 className="page-title">Home</h2>
             <div className="spacer" />
+            <img src="/kekw.gif" id="kekw" />
         </div>
     )
 }
