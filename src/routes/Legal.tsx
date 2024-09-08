8abe24110e2039016fc09b841e56421f89da9430
lsdevcloud <info@lsdevcloud.com>
2024-09-08 14:38:57 +0200

    added legal page

---
+++ src/routes/Legal.tsx
@@ -0,0 +1,15 @@
+export default function Legal() {
+
+
+    return (
+        <div id="page">
+            <h2 className="page-title">Legal</h2>
+            <div className="spacer"></div>
+            <div className="center">
+                <h1>Imprint</h1>
+                <h2><i>Insert legal shit and what not here</i></h2>
+            </div>
+        </div>
+    )
+
+}
